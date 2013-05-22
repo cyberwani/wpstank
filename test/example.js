53ae22fc6e6a2cee69bed68eb833550433d44fc7
Luke Thomas <luk3thomas@gmail.com>
2013-05-22 00:15:37 -0500

    fix tests

--- test/example.js
+++ test/example.js
@@ -1,10 +1,24 @@
 
-var stank = require('../')
+var WPStank = require('../')
+    , stank = new WPStank
     , assert = require('assert')
     , should = require('should');
 
 describe("File System", function(){
     it("Can add a file", function(){
         (1).should.equal(1);
+    });
+});
+
+describe("Templates", function(){
+    describe("default settings", function(){
+        it("has post type", function(){
+            (typeof stank.template.postType).should.eql("string")
+            stank.template.postType.should.not.have.lengthOf(0)
+        });
+        it("has taxonomy", function(){
+            (typeof stank.template.taxonomy).should.eql("string")
+            stank.template.taxonomy.should.not.have.lengthOf(0)
+        });
     });
 });
