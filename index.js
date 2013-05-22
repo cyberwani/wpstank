e0a6e5253f78b9711b8b7e397a4eaf0e8f221927
Luke Thomas <luk3thomas@gmail.com>
2013-05-21 22:44:19 -0500

    move code into entry file

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 #!/usr/bin/env node
 
-require('./src/wpstank.js');
+var program = require('commander'),
+    stank = require('./src/wpstank.js');
+
+program
+    .version('0.0.0')
+    .option('-g, --generate', 'Generate a file')
+    .option('-d, --delete', 'Delete a file')
+    .option('-p, --post-type [type]', 'Post Type')
+    .option('-t, --taxonomy', 'Taxonomy');
+
+program
+    .command('init')
+    .description('Setup stank')
+    .action(function(){
+    });
+
+program.parse( process.argv );
+
+if( program.generate ) {
+    if( program.postType ) {
+        // Is a post type specified?
+        if( program.postType === true )
+            return console.log( "No post type was specified" );
+    }
+}
