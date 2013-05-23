4d232b247a72cbf88597aad1d4b4d4df2cc53f15
Luke Thomas <luk3thomas@gmail.com>
2013-05-23 01:18:57 -0500

    adding tests

--- test/stank.js
+++ test/stank.js
@@ -1,8 +1,26 @@
 
 var WPStank = require('../')
     , stank = new WPStank
-    , assert = require('assert')
+    , fs = require('fs')
     , should = require('should');
+
+process.chdir( __dirname );
+
+describe("Initializer", function(){
+    // remove any files
+    before(function(){
+        stank.file.rm( '.wpstank' )
+    });
+
+    it("Is empty before a test", function(){
+        fs.existsSync( '.wpstank' ).should.eql( false );
+    });
+
+    it("Creates a preference file", function(){
+        stank.init();
+        fs.existsSync( '.wpstank' ).should.eql( true );
+    });
+});
 
 describe("File System", function(){
     it("Can add a file", function(){
@@ -12,11 +30,11 @@ describe("File System", function(){
 
 describe("Templates", function(){
     describe("default settings", function(){
-        it("has post type", function(){
+        it("includes a custom post type", function(){
             (typeof stank.template.postType).should.eql("string")
             stank.template.postType.should.not.have.lengthOf(0)
         });
-        it("has taxonomy", function(){
+        it("includes a taxonomy", function(){
             (typeof stank.template.taxonomy).should.eql("string")
             stank.template.taxonomy.should.not.have.lengthOf(0)
         });
