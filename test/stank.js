87baff731495bd78029fe7911a45325e9ff53151
Luke Thomas <luk3thomas@gmail.com>
2013-05-26 15:48:33 -0500

    add more tests

--- test/stank.js
+++ test/stank.js
@@ -9,16 +9,30 @@ process.chdir( __dirname );
 describe("Initializer", function(){
     // remove any files
     before(function(){
+        stank.file.rm( '.wpstankrc' )
         stank.file.rm( '.wpstank' )
     });
 
     it("Is empty before a test", function(){
-        fs.existsSync( '.wpstank' ).should.eql( false );
+        fs.existsSync( '.wpstankrc' ).should.eql( false );
     });
 
     it("Creates a preference file", function(){
         stank.init();
+        fs.existsSync( '.wpstankrc' ).should.eql( true );
+    });
+
+    it("Creates a preference directory", function(){
+        stank.init();
         fs.existsSync( '.wpstank' ).should.eql( true );
+    });
+
+    it("Directory preference files are templates", function(){
+        stank.init();
+        for( template in stank.template ) {
+            file = fs.readFileSync( '.wpstank/' + template, 'UTF-8' );
+            file.should.eql( stank.template[template] );
+        }
     });
 });
 
@@ -29,7 +43,7 @@ describe("File System", function(){
 });
 
 describe("Templates", function(){
-    describe("default settings", function(){
+    describe("Default settings", function(){
         it("includes a custom post type", function(){
             (typeof stank.template.postType).should.eql("string")
             stank.template.postType.should.not.have.lengthOf(0)
@@ -37,6 +51,11 @@ describe("Templates", function(){
         it("includes a taxonomy", function(){
             (typeof stank.template.taxonomy).should.eql("string")
             stank.template.taxonomy.should.not.have.lengthOf(0)
+        });
+    });
+    describe("Interactions", function(){
+        it("Are read from the preferences dir", function(){
+            stank.get( 'postType' ).should.eql( fs.readFileSync( '.wpstank/postType', 'UTF-8' ) );
         });
     });
 });
